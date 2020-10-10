13fbd24299d0e1a70ca8df4732dd97a69dc5b0b2
Vibhor Singh <vibhor.formal@gmail.com>
2020-10-10 15:48:34 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-
+import {ThemeProvider} from 'styled-components'
+import { theme } from "./themes/theme"
 ReactDOM.render(
-  <React.StrictMode>
+	<ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>
+  ,
   document.getElementById('root')
 );
 
