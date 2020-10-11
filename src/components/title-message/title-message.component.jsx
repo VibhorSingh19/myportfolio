c67fce134915f28ba1888febcfb2da1f5e3da84d
Vibhor Singh <vibhor.formal@gmail.com>
2020-10-11 22:50:38 +0530

    Updated

--- src/components/title-message/title-message.component.jsx
+++ src/components/title-message/title-message.component.jsx
@@ -1,4 +1,32 @@
 import React from 'react';
+import styled from "styled-components";
+
+
+const MyTitleMessage = styled.h1`
+  position: absolute;
+  width: 100%;
+  top: 22rem;
+  z-index: 1;
+  margin-top: -125px;
+  text-align: center;
+  strong {
+    font-size: 1.25em;
+  }
+  div {
+    color: ${(props) => props.theme.textColor};
+    text-shadow: 0px 2px 5px rgba(0, 0, 0, 0.4);
+    font-weight: 100;
+    letter-spacing: 7px;
+    .main {
+      font-size: 50px;
+    }
+    .sub {
+      font-size: 27px;
+      letter-spacing: 2px;
+    }
+  }
+`;
+
 
 const TitleMessage= () => {
   return (
